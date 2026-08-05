09d42bb7a82a9c542c0c09971c33b2846804e872
zet-nbs <fairizet@gmail.com>
2026-08-05 14:31:13 +0700

    Implement folder CRUD functionality with routes and controller

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const passport = require("./config/passport");
 const authRoutes = require("./routes/authRoutes");
+const folderRoutes = require("./routes/folderRoutes");
 
 const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 app.use(passport.initialize());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/folders", folderRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
